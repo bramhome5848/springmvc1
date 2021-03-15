b0547ccfa9688f62c44e3e46ba66e58fe6812cd9
이경진 <bramhome5848@gmail.com>
2021-03-15 14:13:16 +0900

    20210315 HttpServletRequest - 기본 사용법

--- servlet/src/main/java/hello/servlet/basic/HelloServlet.java
+++ servlet/src/main/java/hello/servlet/basic/HelloServlet.java
@@ -1,27 +1,27 @@
 package hello.servlet.basic;
 
 import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "helloServlet", urlPatterns = "/hello")  //servlet name, url mapping
 public class HelloServlet extends HttpServlet {
 
     @Override
-    public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         System.out.println("HelloServlet.service");
-        System.out.println("req = " + req); //org.apache.catalina.connector.RequestFacade@2ddf7e1a
-        System.out.println("res = " + res); //org.apache.catalina.connector.ResponseFacade@5bfc028b
+        System.out.println("req = " + request); //org.apache.catalina.connector.RequestFacade@2ddf7e1a
+        System.out.println("res = " + response); //org.apache.catalina.connector.ResponseFacade@5bfc028b
 
-        String username = req.getParameter("username"); //parameter
+        String username = request.getParameter("username"); //parameter
         System.out.println("username = " + username);   //parameter print
 
-        res.setContentType("text/plain");   //response contentType
-        res.setCharacterEncoding("utf-8");  //response encode -> korean
-        res.getWriter().write("hello " + username); //response to browser
+        response.setContentType("text/plain");   //response contentType
+        response.setCharacterEncoding("utf-8");  //response encode -> korean
+        response.getWriter().write("hello " + username); //response to browser
     }
 }
